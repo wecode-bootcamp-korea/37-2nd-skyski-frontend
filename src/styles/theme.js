0294ed299a41da8773b2322316ad2a19db7dfdf8
JuwonChoi902 <qfnl90@naver.com>
2022-10-04 17:52:30 +0900

    ADD:style 폴더 완료

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  darkBlue: '#02122C',
+  lightBlue: '#0770E3',
+  green: '#00A799',
+  backgroundWhite: '#F1F2F8',
+  white: '#FFFFFF',
+  grey: '#DDDDE5',
+  orange: '#FF7B58',
+};
+
+export default theme;
