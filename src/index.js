c8caa7be00d2363e05843f5526f93960252ae813
JuwonChoi902 <qfnl90@naver.com>
2022-10-04 19:07:27 +0900

    index.js import완료

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
