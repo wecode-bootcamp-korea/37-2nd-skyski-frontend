8d9e271a9769e02c70bffbfb9b948d49cd5ac444
JuwonChoi902 <qfnl90@naver.com>
2022-10-04 18:05:16 +0900

    스타일 컴포넌트 적용

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom/client';
 import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
+root.render(
+  <ThemeProvider theme={{ style: theme, variables }}>
+    <GlobalStyle />
+    <Router />
+  </ThemeProvider>
+);
